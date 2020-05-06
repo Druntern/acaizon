4d9c88364e1cb725b14b2c68cd2eed6f2b3d699b
Druntern <j_henrique_21@hotmail.com>
2020-05-06 23:23:28 +0100

    Mobile side menu

---
+++ public/script.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mobile-nav").style.width = "100%";
+    document.getElementById("mobile-nav").style.opacity = "100%";
+
+}
+  
+function closeNav() {
+    document.getElementById("mobile-nav").style.width = "0";
+    document.getElementById("mobile-nav").style.opacity = "0";
+}
